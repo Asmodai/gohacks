f7dbf7c492ee4003de17571129c701f2efe84344
Paul Ward <asmodai@gmail.com>
2021-11-24 17:54:55 +0000

    Add crypto code.

---
+++ crypto/random.go
@@ -0,0 +1,91 @@
+/*
+ * random.go --- Random string generation.
+ *
+ * Copyright (c) 2021 Paul Ward <asmodai@gmail.com>
+ *
+ * Author:     Paul Ward <asmodai@gmail.com>
+ * Maintainer: Paul Ward <asmodai@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+package crypto
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"math/big"
+)
+
+func assertAvailablePRNG() {
+	buf := make([]byte, 1)
+
+	_, err := io.ReadFull(rand.Reader, buf)
+	if err != nil {
+		panic(
+			fmt.Sprintf(
+				"crypto/rand is unavailable: read() failed with %#v",
+				err,
+			),
+		)
+	}
+}
+
+func GenerateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func GenerateRandomString(n int) (string, error) {
+	const alnum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+	ret := make([]byte, n)
+
+	for i := 0; i < n; i++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(alnum))))
+		if err != nil {
+			return "", err
+		}
+
+		ret[i] = alnum[num.Int64()]
+	}
+
+	return string(ret), nil
+}
+
+func GenerateRandomSafeString(n int) (string, error) {
+	b, err := GenerateRandomBytes(n)
+
+	return base64.URLEncoding.EncodeToString(b), err
+}
+
+func GenerateRandomSafeBytes(n int) ([]byte, error) {
+	b, err := GenerateRandomSafeString(n)
+
+	return []byte(b), err
+}
+
+func init() {
+	assertAvailablePRNG()
+}
+
+/* random.go ends here. */
