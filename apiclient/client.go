c92383cddebc5df1671cb2ffbb2fa392394f3d44
Paul Ward <asmodai@gmail.com>
2022-07-06 10:07:09 +0100

    removed derpy space so that goreportcard shuts up.

--- apiclient/client.go
+++ apiclient/client.go
@@ -12,7 +12,7 @@
  * including without limitation the rights to use, copy, modify, merge,
  * publish, distribute, sublicense, and/or sell copies of the Software,
  * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions: 
+ * subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
@@ -172,7 +172,7 @@ func (c *Client) httpAction(verb string, data *Params) ([]byte, int, error) {
 	if len(data.Queries) != 0 {
 		q := req.URL.Query()
 
-		for idx, _ := range data.Queries {
+		for idx := range data.Queries {
 			q.Add(data.Queries[idx].Name, data.Queries[idx].Content)
 		}
 
--- apiserver/utils.go
+++ apiserver/utils.go
@@ -12,7 +12,7 @@
  * including without limitation the rights to use, copy, modify, merge,
  * publish, distribute, sublicense, and/or sell copies of the Software,
  * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions: 
+ * subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
@@ -26,6 +26,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
 package apiserver
 
 import (
